d8d3c26d284e54c6ed8478030c874c4bc8879db3
Alan Boyle <alan.boyle@madetoengage.com>
2020-02-05 15:25:42 +0000

    Shop page added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <switch>
+      <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={HatsPage}/>
-      </switch>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
