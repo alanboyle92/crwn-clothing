0e5abf6e37f65c265697466f07fc08d2f8eaf7a7
Alan Boyle <alan.boyle@madetoengage.com>
2020-02-07 16:32:44 +0000

    Firebase Added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -49,6 +51,7 @@ class SignIn extends React.Component {
           />
 
           <CustomButton type="submit"> Sign In </CustomButton>
+          <CustomButton onClick={signInWithGoogle}> Sign In With Google </CustomButton>
         </form>
       </div>
     );
